e4abeffa646d25b16d9166ae148a818249970b64
Stephen Hicks <stephenhicks@gmail.com>
2020-05-14 03:56:31 -0700

    widen the table

--- xor.js
+++ xor.js
@@ -3,12 +3,12 @@ import {words, index} from './words.js';
 const word = document.getElementById('word');
 const table = document.getElementById('table');
 
-for (let i = 0; i < (words.length >> 2); i++) {
+for (let i = 0; i < (words.length >> 3); i++) {
   const tr = document.createElement('tr');
-  for (let col = 0; col < 4; col++) {
+  for (let col = 0; col < 8; col++) {
     const left = document.createElement('td');
     const right = document.createElement('td');
-    left.textContent = right.textContent = words[col << 9 | i];
+    left.textContent = right.textContent = words[col << 8 | i];
     tr.appendChild(left);
     tr.appendChild(right);
   }
@@ -18,8 +18,8 @@ for (let i = 0; i < (words.length >> 2); i++) {
 word.addEventListener('change', () => {
   const base = word.values.reduce((a, b) => a ^ b, 0);
   for (let i = 0; i < words.length; i++) {
-    const col = i >> 8 | 1;
-    table.children[i & 0x1ff].children[col].textContent = words[i ^ base];
+    const col = i >> 7 | 1;
+    table.children[i & 0xff].children[col].textContent = words[i ^ base];
   }
 });
 
